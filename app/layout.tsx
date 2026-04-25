823391ee4565edde697fa278c93cb2a8c7627ebf
diguinddtank-code <diguin.ddtank@gmail.com>
2026-04-25 12:08:16 -0300

    feat: Centralize Meta Pixel tracking code
    Move the Meta Pixel tracking code from the `CareersClient` component to the root `layout.tsx` file. This ensures the pixel is initialized on all pages of the application, providing more comprehensive analytics.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Montserrat, Open_Sans, Dancing_Script } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 const montserrat = Montserrat({
@@ -91,6 +92,24 @@ export default function RootLayout({
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
       </head>
       <body className="antialiased font-sans">
+        {/* Meta Pixel Code */}
+        <Script id="meta-pixel" strategy="afterInteractive" dangerouslySetInnerHTML={{ __html: `
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '2259353031142127');
+          fbq('track', 'PageView');
+        ` }} />
+        <noscript>
+          <img height="1" width="1" style={{display: 'none'}} src="https://www.facebook.com/tr?id=2259353031142127&ev=PageView&noscript=1" alt="" />
+        </noscript>
+        {/* End Meta Pixel Code */}
+
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
